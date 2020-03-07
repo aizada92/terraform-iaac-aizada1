13ad39d4f83da22e2ecbdb4cfc1cc26068b042a0
Aizada Kanybekkyzy <aizadushka87@gmail.com>
2020-03-07 16:14:01 -0600

    addedd dybomodb

--- backend.tf
+++ backend.tf
@@ -4,6 +4,6 @@
     bucket = "terraform-class-aizada"
     key    = "path/to/my/key"
     region = "us-east-1"
-    dynomodb_table = "terraform-class"
+    dynamodb_table = "terraform-class"
   }
 }
