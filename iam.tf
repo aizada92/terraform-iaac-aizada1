3fa47f1237d0d5f51542f99c73bdb03c48338582
Aizada Kanybekkyzy <aizadushka87@gmail.com>
2020-03-07 14:39:04 -0600

    save

--- iam.tf
+++ iam.tf
@@ -29,7 +29,9 @@ resource "aws_iam_group_membership" "developers_team" {
   name = "developers-group-membership" 
   users = [ 
    "${aws_iam_user.tim.name}"
-    "${aws_iam_user.ben.names}"
+   
+    "${aws_iam_user.ben.name}"
+
     "${aws_iam_user.lisa.name}" 
   ] 
   group = "${aws_iam_group.developers.name}" 
